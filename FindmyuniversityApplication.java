5e361ab58383116f8bfac2ee3cf5cb9b3cae27ba
PAULCHANDA <129196672+PAULCHANDA@users.noreply.github.com>
2024-08-21 17:39:27 +0200

    Add files via upload

---
+++ FindmyuniversityApplication.java
@@ -0,0 +1,13 @@
+package org.mycompany.findmyuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmyuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmyuniversityApplication.class, args);
+	}
+
+}
---
+++ FindmyuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mycompany.findmyuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
